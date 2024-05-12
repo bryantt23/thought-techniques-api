59138d5def5dcfe030ff2aee7b1bdb16801f4d1b
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-05-12 01:00:46 -0700

    connect to mongodb

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require("express")
+const { MongoClient, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 const app = express()
+app.use(express.json())
 const PORT = 3000
 
-app.use(express.json())
+const uri = process.env.MONGODB_URI
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+app.get("/techniques", async (req, res) => {
+    try {
+        await client.connect();
+        const collection = client.db("thought-smasher").collection("techniques")
+        const techniques = await collection.find({}).toArray()
+        res.json(techniques)
+    } catch (error) {
+        console.log(error)
+    } finally {
+        await client.close()
+    }
+})
 
 app.get("/hello", (req, res) => {
     res.json({ message: "Hello world" })
